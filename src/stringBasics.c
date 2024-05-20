7c0ae591ae0a9a0ca648510514df2af8a4a953da
chadd williams <will4614@pacificu.edu>
2024-05-20 22:39:33 +0000

    code stringBasics

--- src/stringBasics.c
+++ src/stringBasics.c
@@ -10,6 +10,40 @@
 #include <stdlib.h>
 #include <string.h>
 
+# define LETTERS_SIZE 3
+
+
+int functionWithUnitializedCharArray (int update)
+{
+	const int SIZE = 10;
+	char szUnitializedLocalArray[SIZE];
+	int value;
+
+	value += update;
+	return value;
+}
+
+int functionWithLotsOfLocalVars(int update)
+{
+	const int SIZE = 100;
+	int intArray[SIZE];
+	char szUnitializedLocalArray[SIZE];
+	int value;
+	double doubleNumber;
+	int secondIntArray[SIZE];
+
+	value += update;
+	memset(intArray, 0x11, SIZE*sizeof(int));
+	memset(secondIntArray, 0x13, SIZE*sizeof(int));
+	memset(szUnitializedLocalArray, 0x11, SIZE*sizeof(char));
+
+	doubleNumber = 100.8 * value;
+	value -= doubleNumber;
+	return value;
+}
+
+
+
  /****************************************************************************
   Function: 	 main
 
@@ -22,7 +56,52 @@
   ****************************************************************************/
 int main (int argc, char* argv[]) {
 
-	
-  
-  return EXIT_SUCCESS;
+	const int SIZE = 10;
+	const int ARRAY_SIZE = 4;
+	char szLetters[ARRAY_SIZE];
+	char letters[LETTERS_SIZE] = {'A', 'B', 'C'};
+	char szName[SIZE];
+	char szUnitialized[SIZE];
+
+	// szLetters = "ABC";
+
+	strncpy(szLetters, "ABC", ARRAY_SIZE);
+
+	printf("szLetter, a null terminated character array:\n");
+	printf("%c", szLetters[1]);
+	printf("\n\n");
+  printf("%s", szLetters);
+
+	printf("\n\n");
+	printf("letters, a character array:\n");
+	printf("%c", letters[1]);
+	printf("\n\n");
+	printf("%s", letters);
+
+	/////////////////////////
+	printf("\n\nszName, after using strncpy():\n");
+	strncpy(szName, "CS300", SIZE);
+	printf("\n%ld %s\n\n", strlen(szName), szName);
+
+	printf("Using scanf to read a string: ");
+	scanf("%s", szName);
+	printf("%s\n", szName);
+
+	// scanf leaves the \n in the keyboard buffer so
+	// we need to read it before we use fgets() or getline()
+	getc(stdin); 
+
+	printf("Using fgets to read a string: ");
+	fgets(szName, SIZE, stdin);
+	printf(">%s<\n", szName);
+
+
+	printf("Printing an unitialized string: ");
+	printf(">%s<\n", szUnitialized);
+
+	functionWithUnitializedCharArray(2);
+	functionWithLotsOfLocalVars(11);
+	functionWithUnitializedCharArray(4);
+
+	return EXIT_SUCCESS;
 }
