f4cbcbf22d90c0780f425953f2bf4a0f4bf2d196
chadd williams <will4614@pacificu.edu>
2024-05-23 20:15:36 +0000

    add practieFileReading.c

--- Makefile
+++ Makefile
@@ -28,7 +28,7 @@ ENSCRIPT_FLAGS=-C -T 2 -p - -M Letter -Ec --color -fCourier8
 # other than the newline after the \.  A blank space after the \ gives errors. 
 
 TARGETS=bin/stringBasics bin/practiceStringBasics bin/stringSolution1 \
-bin/fileReading \
+bin/fileReading bin/practiceFileReading \
 bin/stringFunctions bin/stringSolution2 bin/fileSolution1 bin/fileSolution2
 
 all: bin ${TARGETS}
---
+++ src/practiceFileReading.c
@@ -0,0 +1,26 @@
+/****************************************************************************
+ File name:  	practiceFileReading.c
+ Author:
+ Date:
+ Class:				CS 300
+ Assignment:	C File Reading Example Videos
+ Purpose:			practice C File Reading
+ ****************************************************************************/
+#include <stdio.h>
+
+ /****************************************************************************
+  Function: 	 	main
+
+  Description: 	practice file reading
+
+  Parameters:		argc - the number of command line arguments
+              	argv - the actual command line arguments
+
+  Returned:	 	EXIT_SUCCESS
+  ****************************************************************************/
+int main (int argc, char* argv[]) {
+
+
+  
+  return 0; // EXIT_SUCCESS
+}
